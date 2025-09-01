c95e5ca87c8b9ef4250ef4db8b052d24863339fe
Guilherme Monteiro <guibezerra_monteiro@hotmail.com>
2025-09-01 15:42:43 -0300

    feat(log_config): add log condfig

--- app/core/log_config.py
+++ app/core/log_config.py
@@ -1,6 +1,56 @@
 import sys
+import time
 
+import httpx
 from loguru import logger
+
+from app.core.config import settings
+
+INFO_LEVEL = 20
+
+
+def send_log_to_loki(record: dict):
+    """Envia um log formatado para o Loki."""
+    timestamp = str(int(time.time() * 1_000_000_000))  # nanoseconds
+
+    # montando labels para Loki
+    labels = {
+        "language": "python",
+        "source": "fastapi",
+        "level": record["level"].name,
+        "file": record["file"].name,
+        "function": record["function"],
+    }
+
+    # O valor será o log inteiro formatado
+    log_line = (
+        f"{record['time'].strftime('%Y-%m-%d %H:%M:%S')} | "
+        f"{record['level'].name:<8} | "
+        f"{record['file'].name}:{record['line']} | {record['function']}() | "
+        f"{record['message']}"
+    )
+
+    payload = {
+        "streams": [
+            {
+                "stream": labels,
+                "values": [[timestamp, log_line]],
+            }
+        ]
+    }
+
+    headers = {"Content-Type": "application/json"}
+    try:
+        resp = httpx.post(
+            url=settings.LOKI_URL,
+            auth=(settings.LOKI_USER_ID, settings.LOKI_TOKEN),
+            json=payload,
+            headers=headers,
+            timeout=5.0,
+        )
+        resp.raise_for_status()
+    except httpx.RequestException as e:
+        logger.opt(exception=True).error(f"Erro ao enviar log para Loki: {e}")
 
 
 def setup_logging():
@@ -21,16 +71,24 @@ def setup_logging():
         catch=True,
     )
 
-    # json file sink, good for Loki
+    # json file sink
     logger.add(
-        "logs.json",
-        level="INFO",  # geralmente Loki consome INFO+
-        serialize=True,  # JSON estruturado
+        "logs/logs.json",
+        level="INFO",
+        serialize=True,
         enqueue=True,
         catch=True,
         rotation="10 MB",
         retention="7 days",
         compression="zip",
     )
+
+    # Enviar INFO+ para o Loki com todos os detalhes
+    def loguru_to_loki(message):
+        record = message.record
+        if record["level"].no >= INFO_LEVEL:
+            send_log_to_loki(record)
+
+    logger.add(loguru_to_loki, level="INFO", enqueue=True, catch=True)
 
     return logger
