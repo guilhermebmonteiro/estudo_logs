388200e6e250992e0bc45028eb7473a7f105d60a
Guilherme Monteiro <guibezerra_monteiro@hotmail.com>
2025-09-01 15:34:39 -0300

    fix(pydantics-settings): add.env variables to config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,26 @@
-from pydantic import BaseSettings
+from typing import List, Optional
+
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     ENVIRONMENT: str = "local"
-    LOG_LEVEL: str = "INFO"
+
+    LOKI_URL: str
+    LOKI_TOKEN: str
+
+    SENTRY_DSN: Optional[str] = None
+
+    LOKI_USER_ID: str
+
+    PROJECT_NAME: str
+    API_V1_STR: str
+    LOG_LEVEL: str
+    ALL_CORS_ORIGINS: Optional[List[str]]  # aceita lista de strings
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True  # mantém case sensitive
 
 
 settings = Settings()
